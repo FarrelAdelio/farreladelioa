b9eefe8cee24500e1c02dec8e82aa21f0f0b4ad7
FarrelAdelio <109320442+FarrelAdelio@users.noreply.github.com>
2022-11-06 22:03:20 +0700

    need nav

---
+++ functions.js
@@ -0,0 +1,34 @@
+// FILTER FUNCTION
+const liItem = document.querySelectorAll('ul li');
+const imgItem = document.querySelectorAll('.filter-items .list');
+
+liItem.forEach(li => {
+    li.onclick = function () {
+        //active
+        liItem.forEach(li => {
+            li.className = "text filter-btn button-content";
+        })
+        li.className = "active text filter-btn button-content";
+
+        //Filter
+        const value = li.textContent;
+        imgItem.forEach(section => {
+            section.style.display = 'none';
+            if (section.getAttribute('data-item') == value.toLowerCase() ||
+                value == "personal") {
+                section.style.display = 'flex';
+            }
+        })
+    }
+});
+// FILTER FUNCTION END
+
+// MENU TOGGLE FUNCITON
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener('click', function () {
+    nav.classList.toggle("slide");
+})
+
+// MENU TOGGLE END
